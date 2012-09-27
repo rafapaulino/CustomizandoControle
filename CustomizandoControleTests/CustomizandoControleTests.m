172413bb8b6e0b04b072308c3c8b3bf6d1c00ff2
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-27 11:25:16 -0300

    Initial Commit

---
+++ CustomizandoControleTests/CustomizandoControleTests.m
@@ -0,0 +1,32 @@
+//
+//  CustomizandoControleTests.m
+//  CustomizandoControleTests
+//
+//  Created by Rafael Brigagão Paulino on 27/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "CustomizandoControleTests.h"
+
+@implementation CustomizandoControleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CustomizandoControleTests");
+}
+
+@end
